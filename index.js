ac1a3a0cb7a56cb3e8eb65eb79b475b645fb671a
gwendal le bris <gwen.coding@gmail.com>
2023-11-01 12:05:59 +0100

    Lesson: Importing & Exporting Modules

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 #!/usr/bin/env node
 
-import { count } from './utils.js';
+import { count } from './utils.js'; // start with dot to indicate that it's a relative path
 import whatever from './utils.js';
+import fs from 'fs'; // fs is a built-in module // no specify path because it's a built-in module
+
+// Equivalent with CommonJS : require
+// const { count } = require('./utils.js');
+// const whatever = require('./utils.js');
+// const fs = require('fs');
 
 console.log(count(1));
 console.log(whatever);
--- utils.js
+++ utils.js
@@ -1,3 +1,7 @@
 export const count = num => num + 1;
 
 export default { name: "cookies" };
+
+// Equivalent with CommonJS : module.exports
+// module.exports = { count };
+// module.exports = whatever;
