29e2bb72b2169ae685187470401ea1262dfa06bd
gwendal le bris <gwen.coding@gmail.com>
2023-11-01 16:23:02 +0100

    CRUD Methods: Read & Delete

--- src/notes.js
+++ src/notes.js
@@ -1,5 +1,7 @@
 import { insertDB, saveDB, getDB } from "./db";
 
+// Create a new note
+
 export const newNote = async (note, tags) => {
   const newNote = {
     tags,
@@ -8,4 +10,37 @@ export const newNote = async (note, tags) => {
   }
   await insertDB(newNote);
   return newNote;
-}
+};
+
+// Get all notes
+
+export const getAllNotes = async () => {
+  const { notes } = await getDB();
+  return notes;
+};
+
+// Find notes by text
+
+export const findNote = async (filter) => {
+  const { notes } = await getDB();
+  return notes.filter(note => note.content.toLowercase().includes(filter.toLowercase()));
+};
+
+// Delete note by id
+
+export const removeNote = async (id) => {
+  const { notes } = await getDB();
+  const match = notes.find(note => note.id === id);
+
+  if (!match) {
+    throw new Error(`Note with id ${id} not found`);
+  } else {
+    const newNotes = notes.filter(note => note.id !== id);
+    await saveDB({ notes: newNotes });
+    return match;
+  }
+};
+
+// Remove all notes
+
+export const removeAllNotes = () => saveDB({ notes: [] });
