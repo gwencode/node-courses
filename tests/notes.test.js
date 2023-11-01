84952705b815b9b737b37f1542b2647ebc17ef47
gwendal le bris <gwen.coding@gmail.com>
2023-11-01 18:59:03 +0100

    Testing with Mocks

--- tests/notes.test.js
+++ tests/notes.test.js
@@ -1,6 +1,37 @@
-const add = (num, num2) => num + num2;
+// const add = (num, num2) => num + num2;
 
-test('add takes two numbers and returns a sum', () => {
-  const result = add(1, 2);
-  expect(result).toBe(3);
+// test('add takes two numbers and returns a sum', () => {
+//   const result = add(1, 2);
+//   expect(result).toBe(3);
+// })
+
+import { jest } from '@jest/globals';
+
+jest.unstable_mockModule('../src/db.js', () => ({
+  insertDB: jest.fn(),
+  getDB: jest.fn(),
+  saveDB: jest.fn(),
+}));
+
+const { insertDB, getDB, saveDB } = await import('../src/db.js');
+const { newNote, getAllNotes, removeNote } = await import('../src/notes.js');
+
+beforeEach(() => {
+  insertDB.mockClear();
+  getDB.mockClear();
+  saveDB.mockClear();
 })
+
+test('newNote inserts data and returns it', async () => {
+  const note = 'Test note';
+  const tags = ['tag1', 'tag2'];
+  const data = {
+    tags,
+    content: note,
+    id: Date.now(),
+  };
+  insertDB.mockResolvedValue(data);
+
+  const result = await newNote(note, tags);
+  expect(result).toEqual(data);
+});
