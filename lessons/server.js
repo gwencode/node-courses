0a20c9f93fe344ee6a8e415230a32357c213a510
gwendal le bris <gwen.coding@gmail.com>
2023-11-01 19:41:48 +0100

    Creating a Basic Server

---
+++ lessons/server.js
@@ -0,0 +1,12 @@
+// Convert ligne above to ES
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!');
+});
+
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
