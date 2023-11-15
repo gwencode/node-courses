8130ac90514d8da8be94889f69e184db6bc2fce7
Gwendal Le Bris <gwen.coding@gmail.com>
2023-11-15 17:24:06 +0100

    Lesson: Argument Handlind

--- digging-into-node/exercises/ex1.js
+++ digging-into-node/exercises/ex1.js
@@ -2,17 +2,32 @@
 
 "use strict";
 
-printHelp();
+const args = require("minimist")(process.argv.slice(2), {
+  boolean: ["help"],
+  string: ["file"]
+})
 
-// // process.stdout.write("Hello World");
-// // process.stdout.write("Hello World\n");
+if (args.help) {
+  printHelp();
+} else if (args.file) {
+  console.log(args.file);
+} else {
+  error("Incorrect usage.", true);
+}
 
-// console.log("Hello world");
+function error(msg, includeHelp = false) {
+  console.error(msg);
+  if (includeHelp) {
+    console.log("");
+    printHelp();
+  }
+}
 
-// console.error("Oops");
+// ********************
 
 function printHelp() {
 	console.log("ex1 usage:");
+	console.log("  ex1 --file={FILENAME}:");
 	console.log("");
 	console.log("--help                      print this help");
 	console.log("-, --in                     read file from stdin");
@@ -20,3 +35,10 @@ function printHelp() {
 	console.log("");
 	console.log("");
 }
+
+  // // process.stdout.write("Hello World");
+  // // process.stdout.write("Hello World\n");
+
+  // console.log("Hello world");
+
+  // console.error("Oops");
