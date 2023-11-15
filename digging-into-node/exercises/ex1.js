e7ebef3195e4033162a5fac0579ac67b1a8d9e02
Gwendal Le Bris <gwen.coding@gmail.com>
2023-11-15 17:06:46 +0100

    Setting Up a Command Line Script

--- digging-into-node/exercises/ex1.js
+++ digging-into-node/exercises/ex1.js
@@ -1,6 +1,22 @@
-// process.stdout.write("Hello World");
-// process.stdout.write("Hello World\n");
+#!/usr/bin/env node
 
-console.log("Hello world");
+"use strict";
 
-console.error("Oops");
+printHelp();
+
+// // process.stdout.write("Hello World");
+// // process.stdout.write("Hello World\n");
+
+// console.log("Hello world");
+
+// console.error("Oops");
+
+function printHelp() {
+	console.log("ex1 usage:");
+	console.log("");
+	console.log("--help                      print this help");
+	console.log("-, --in                     read file from stdin");
+	console.log("--file={FILENAME}           read file from {FILENAME}");
+	console.log("");
+	console.log("");
+}
