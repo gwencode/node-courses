b76d8fda4319fa3771fd61c06f6a1f60cad3f765
Gwendal Le Bris <gwen.coding@gmail.com>
2023-11-15 16:31:18 +0100

    Lesson: Console Error & Process stderr

--- digging-into-node/exercises/ex1.js
+++ digging-into-node/exercises/ex1.js
@@ -1,6 +1,6 @@
-// My first Node.js program
+// process.stdout.write("Hello World");
+// process.stdout.write("Hello World\n");
 
-process.stdout.write("Hello World");
-process.stdout.write("Hello World\n");
+console.log("Hello world");
 
-// console.log("Hello world");
+console.error("Oops");
