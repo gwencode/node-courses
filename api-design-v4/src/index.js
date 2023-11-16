9d4b1d92d1cdcb06462c17a40559359ecb3ba552
Gwendal Le Bris <gwen.coding@gmail.com>
2023-11-16 18:16:01 +0100

    Lesson: Creating an HTTP Server

--- api-design-v4/src/index.js
+++ api-design-v4/src/index.js
@@ -1,1 +1,12 @@
-const http = require("http");
+import http from "http";
+
+const server = http.createServer((req, res) => {
+  if (req.method === "GET" && req.url === "/") {
+    res.statusCode = 200;
+    res.end("Hello World yah");
+  }
+});
+
+server.listen(3001, () => {
+  console.log("Server running on http://localhost:3001");
+});
