b264ff5abfde092ac5457255fc83d0dbddb81f22
Gwendal Le Bris <gwen.coding@gmail.com>
2023-11-16 15:47:31 +0100

    Child Processes Exercice

--- digging-into-node/exercises/ex7.js
+++ digging-into-node/exercises/ex7.js
@@ -9,7 +9,7 @@ var childProc = require("child_process");
 // ************************************
 
 const HTTP_PORT = 8039;
-// const MAX_CHILDREN = 5;
+const MAX_CHILDREN = 100;
 
 var delay = util.promisify(setTimeout);
 
@@ -20,10 +20,34 @@ main().catch(console.error);
 // ************************************
 
 async function main() {
-	// console.log(`Load testing http://localhost:${HTTP_PORT}...`);
+	console.log(`Load testing http://localhost:${HTTP_PORT}...`);
 
-  var child = childProc.spawn("node", ["ex7-child.js"]);
-  child.on("exit", (code) => {
-    console.log("Child finished ", code)
-  })
+  while(true) {
+    process.stdout.write(`Sending ${MAX_CHILDREN} requests...`)
+
+  let children = [];
+
+  for (let i = 0; i < MAX_CHILDREN; i++) {
+    children.push(childProc.spawn("node",["ex7-child.js"]))
+  }
+
+  let resps = children.map(function wait(child) {
+    return new Promise(function c(res){
+      child.on("exit", function(code){
+        if (code === 0) res(true);
+        res(false);
+      });
+    });
+  });
+
+  resps = await Promise.all(resps);
+
+  if (resps.filter(Boolean).length === MAX_CHILDREN) {
+    console.log("success!")
+  } else {
+    console.log("failures!")
+  }
+
+  await delay(500);
+  }
 }
