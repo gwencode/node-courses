a667cad5d2f66d600f1e47447d496f8a56e2ddf9
Gwendal Le Bris <gwen.coding@gmail.com>
2023-11-16 16:12:43 +0100

    Debuggin with chrome dev tools

--- digging-into-node/exercises/ex7.js
+++ digging-into-node/exercises/ex7.js
@@ -9,7 +9,7 @@ var childProc = require("child_process");
 // ************************************
 
 const HTTP_PORT = 8039;
-const MAX_CHILDREN = 100;
+const MAX_CHILDREN = 12;
 
 var delay = util.promisify(setTimeout);
 
@@ -21,33 +21,38 @@ main().catch(console.error);
 
 async function main() {
 	console.log(`Load testing http://localhost:${HTTP_PORT}...`);
+  var x = 0;
 
   while(true) {
+    x++;
     process.stdout.write(`Sending ${MAX_CHILDREN} requests...`)
+    let children = [];
 
-  let children = [];
+    for (let i = 0; i < MAX_CHILDREN; i++) {
+      children.push(childProc.spawn("node",["ex7-child.js"]))
+    }
 
-  for (let i = 0; i < MAX_CHILDREN; i++) {
-    children.push(childProc.spawn("node",["ex7-child.js"]))
-  }
-
-  let resps = children.map(function wait(child) {
-    return new Promise(function c(res){
-      child.on("exit", function(code){
-        if (code === 0) res(true);
-        res(false);
+    let resps = children.map(function wait(child) {
+      return new Promise(function c(res){
+        child.on("exit", function(code){
+          if (code === 0) res(true);
+          res(false);
+        });
       });
     });
-  });
 
-  resps = await Promise.all(resps);
+    if (x > 5) {
+      foo()
+    }
 
-  if (resps.filter(Boolean).length === MAX_CHILDREN) {
-    console.log("success!")
-  } else {
-    console.log("failures!")
-  }
+    resps = await Promise.all(resps);
 
-  await delay(500);
+    if (resps.filter(Boolean).length === MAX_CHILDREN) {
+      console.log("success!")
+    } else {
+      console.log("failures!")
+    }
+
+    await delay(500);
   }
 }
