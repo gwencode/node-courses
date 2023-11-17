e5f41ead16cd33b257b9a16e3bb31912afacb72c
Gwendal Le Bris <gwen.coding@gmail.com>
2023-11-17 11:54:47 +0100

    Lesson: Validating a Bearer Token

--- api-design-v4/src/modules/auth.ts
+++ api-design-v4/src/modules/auth.ts
@@ -19,4 +19,23 @@ export const protect = (req, res, next) => {
     res.json({ message: "not authorized" });
     return;
   }
+
+  const token = bearer.split(" ")[1]; // bearer="Bearer <token>"
+
+  if (!token) {
+    res.status(401);
+    res.json({ message: "not authorized" });
+    return;
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(401);
+    res.json({ message: "not valid token" });
+    return;
+  }
 };
