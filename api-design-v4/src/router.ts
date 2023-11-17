0605923153e4ee8b38fd4f2a6dd1f2459352438c
Gwendal Le Bris <gwen.coding@gmail.com>
2023-11-17 19:00:55 +0100

    Lesson: Adding Validation to Routes

--- api-design-v4/src/router.ts
+++ api-design-v4/src/router.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { body, validationResult } from "express-validator";
 
 const router = Router();
 
@@ -7,7 +8,13 @@ router.get("/product", (req, res) => {
   res.json({ message: "Hello world" });
 });
 router.get("/product/:id", () => {});
-router.put("/product/:id", () => {});
+router.put("/product/:id", body("name").isString(), (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(400).json({ errors: errors.array() });
+    return;
+  }
+});
 router.post("/product", () => {});
 router.delete("/product/:id", () => {});
 
