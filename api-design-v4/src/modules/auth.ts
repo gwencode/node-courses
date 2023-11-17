5f922985189db6fd8107eadeeb0b1d5a77359873
Gwendal Le Bris <gwen.coding@gmail.com>
2023-11-17 11:19:30 +0100

    Lesson: Creating a JSON Web Token

---
+++ api-design-v4/src/modules/auth.ts
@@ -0,0 +1,12 @@
+import jwt from "jsonwebtoken";
+
+export const createJWT = (user) => {
+  const token = jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+    },
+    process.env.JWT_SECRET
+  );
+  return token;
+};
