46988f1b948d4cdd30e92f673d909b8714870fe3
Gwendal Le Bris <gwen.coding@gmail.com>
2023-11-21 10:28:23 +0100

    Lesson: Creating Environment Configurations

--- api-design-v4/src/index.ts
+++ api-design-v4/src/index.ts
@@ -1,7 +1,8 @@
 import * as dotenv from "dotenv";
 dotenv.config();
+import config from "./config";
 import app from "./server";
 
-app.listen(3001, () => {
-  console.log("Server is listening on http://localhost:3001");
+app.listen(config.port, () => {
+  console.log(`Server is listening on http://localhost:${config.port}`);
 });
