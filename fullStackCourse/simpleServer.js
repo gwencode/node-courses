e35b16b4fef17bbd5595460ddfd84e2ef080c6ff
Gwendal Le Bris <gwen.coding@gmail.com>
2023-12-11 20:51:38 +0100

    Do all course

---
+++ fullStackCourse/simpleServer.js
@@ -0,0 +1,11 @@
+const http = require("http");
+const fs = require("fs");
+const PORT = 3000;
+
+const server = http.createServer(function (req, res) {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  fs.createReadStream('index.html').pipe(res);
+});
+
+server.listen(PORT);
+console.log(`Server running at http://localhost:${PORT}`);
